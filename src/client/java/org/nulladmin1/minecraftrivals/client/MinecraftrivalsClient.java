9ab0525930a8dabecbd3dbc0342fd3bedf66b376
nulladmin1 <shrey.deogade@protonmail.com>
2025-05-22 22:59:19 -0400

    Add Java files for Mod

---
+++ src/client/java/org/nulladmin1/minecraftrivals/client/MinecraftrivalsClient.java
@@ -0,0 +1,10 @@
+package org.nulladmin1.minecraftrivals.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftrivalsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/nulladmin1/minecraftrivals/Minecraftrivals.java
@@ -0,0 +1,11 @@
+package org.nulladmin1.minecraftrivals;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Minecraftrivals implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        ModItems.initialize();
+    }
+}
